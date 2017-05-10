24befd5ffaa0805782408944d76dd156b881cc32
tedc <egrandinetti@gmail.com>
2017-05-10 18:11:27 +0200

    page template

--- templates/page-header.php
+++ templates/page-header.php
@@ -3,7 +3,7 @@
 		$sm = '"tween":{"y" : -120},"triggerElement":"#header","triggerHook":0,"duration":"150vh","offset":80';
 ?>
 
-<header class="header header--grid header--grow-md" id="header">
+<header class="header header--grid header--grow-<?php echo is_page_template('template-contact.php') ? 'lg' : 'md'; ?>" id="header">
 	<div class="header__cell header__cell--s6 header__cell--<?php the_field('image_position'); ?><?php if(get_field('is_gallery')): echo ' header__cell--slider'; endif; ?>"<?php if(has_post_thumbnail()):?> style="background-image:url(<?php the_post_thumbnail_url('full'); ?>)"<?php endif; ?><?php scrollmagic($sm); ?>>
 		<?php 
 			if(get_field('is_gallery') && get_field('header_gallery')) :
