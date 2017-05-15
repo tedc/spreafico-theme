53a647241f0dae6362aabda20a9b6e02c6bec017
tedc <egrandinetti@gmail.com>
2017-05-15 18:53:30 +0200

    remove col from cat

--- templates/form.php
+++ templates/form.php
@@ -1,12 +1,14 @@
 <?php get_template_part( 'templates/form', 'header' ); 
 $current_url = home_url(add_query_arg(array(),$wp->request)); ?>
 <div class="form form--grid" ng-form>
+	<?php if($obj != __('Richiesta di preventivo', 'sprfc')) : ?>
 	<div class="form__cell form__cell--s6">
 		<div class="form__content form__content--shrink form__content--grow-md">
 			<?php the_content(); ?>
 		</div>
 	</div>
-	<form class="form__cell form__cell--inputs form__cell--s6" ng-submit="onSubmit(contactForm.$valid, '<?php echo $current_url; ?>')" name="contactForm" novalidate>
+<?php endif; ?>
+	<form class="form__cell form__cell--inputs<?php if($obj != __('Richiesta di preventivo', 'sprfc')) : ?> form__cell--s6<?php endif; ?>" ng-submit="onSubmit(contactForm.$valid, '<?php echo $current_url; ?>')" name="contactForm" novalidate>
 		<input type="hidden" ng-model="formData.obj" ng-init="formData.obj = '<?php echo $obj; ?>'">
 		<input type="text" name="sender" ng-model="formData.sender" placeholder="<?php _e('Nome e cognome (obbligatorio)', 'sprfc'); ?>" required />
 		<input type="email" name="email" ng-model="formData.email" placeholder="<?php _e('Indirizzo email (obbligatorio)', 'sprfc'); ?>" required />
