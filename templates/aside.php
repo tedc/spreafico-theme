f557613f47895b23e0ec184da66daa797eb81237
tedc <egrandinetti@gmail.com>
2017-05-23 20:58:08 +0200

    blog

--- templates/aside.php
+++ templates/aside.php
@@ -1,6 +1,6 @@
 <?php use Roots\Sage\Titles; ?>
 <aside class="section__cell section__cell--aside-s4 section__cell--aside section__cell--aside-shrink"<?php square('blog'); ?>>
-	<h1 class="section__title section__title--huge"><strong><? _e('Blog', 'sprfc'); ?></strong></h1>
+	<h1 class="section__title section__title--huge"><strong><?php _e('Blog', 'sprfc'); ?></strong></h1>
 	<ul class="list list--grow-lg">
 	<?php wp_list_categories(array('title_li'=>0)); ?>
 	</ul>
