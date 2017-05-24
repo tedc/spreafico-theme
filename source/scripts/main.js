f1ead733b06c5b505f3c7fa97f6ef525c5deb019
tedc <egrandinetti@gmail.com>
2017-05-24 20:16:43 +0200

    send

--- source/scripts/main.js
+++ source/scripts/main.js
@@ -36538,6 +36538,9 @@ module.exports = function($window) {
   return ps = {
     link: function(scope, element, attrs) {
       var jqWindow, update;
+      if (isMobile) {
+        return;
+      }
       jqWindow = angular.element($window);
       scope.$evalAsync(function() {
         Ps.initialize(element[0], {
