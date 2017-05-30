436f4c295d7f068ac04e36deb7e62e1ceecbe408
tedc <egrandinetti@gmail.com>
2017-05-30 17:54:38 +0200

    pixel

--- extras/scripts.php
+++ extras/scripts.php
@@ -29,18 +29,17 @@
 		<script>
 		!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
 		n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
-		n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+		n.push=n;n.loaded=!0;n.version=‘2.0’;n.queue=[];t=b.createElement(e);t.async=!0;
 		t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
-		document,'script','https://connect.facebook.net/en_US/fbevents.js');
-		fbq('init', '419852718401373', {
-		em: 'insert_email_variable'
-		});
-		fbq('track', 'PageView');
+		document,‘script’,‘https://connect.facebook.net/en_US/fbevents.js');
+		fbq(‘init’, ‘646591368878022’); // Insert your pixel ID here.
+		fbq(‘track’, ‘PageView’);
 		</script>
-		<noscript><img height="1" width="1" style="display:none"
-		src="https://www.facebook.com/tr?id=419852718401373&ev=PageView&noscript=1"
+		<noscript><img height=“1” width=“1" style=“display:none”
+		src=“https://www.facebook.com/tr?id=646591368878022&ev=PageView&noscript=1"
 		/></noscript>
 		<!-- DO NOT MODIFY -->
+		<!-- End Facebook Pixel Code -->
 	<?php }
 
 	add_action( 'wp_footer', 'vars');
