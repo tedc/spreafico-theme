377d7db9551746974730e9b4088346953df47914
tedc <egrandinetti@gmail.com>
2017-07-26 17:15:24 +0200

    phone adwords

--- extras/scripts.php
+++ extras/scripts.php
@@ -7,6 +7,7 @@
 	function sliderVar() { ?>
 		<script>
 				sliders = [];
+				(function(a,e,c,f,g,h,b,d){var k={ak:"971975964",cl:"-f7sCMj9snMQnNq8zwM"};a[c]=a[c]||function(){(a[c].q=a[c].q||[]).push(arguments)};a[g]||(a[g]=k.ak);b=e.createElement(h);b.async=1;b.src="//www.gstatic.com/wcm/loader.js";d=e.getElementsByTagName(h)[0];d.parentNode.insertBefore(b,d);a[f]=function(b,d,e){a[c](2,b,k,d,null,new Date,e)};a[f]()})(window,document,"_googWcmImpl","_googWcmGet","_googWcmAk","script");
 		</script>
 	<?php }
 	add_action('wp_head', 'sliderVar');
