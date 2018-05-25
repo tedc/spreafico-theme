29d7af96bd5836cc246beae1161866c9ccbae740
tedc <egrandinetti@gmail.com>
2018-05-25 15:11:37 +0200

    -<?php echo $type; ?>

--- templates/form.php
+++ templates/form.php
@@ -14,8 +14,8 @@ $current_url = home_url(add_query_arg(array(),$wp->request)); ?>
 		<input type="email" name="email" ng-model="formData.email" placeholder="<?php _e('Indirizzo email (obbligatorio)', 'sprfc'); ?>" required />
 		<input type="tel" name="tel" ng-model="formData.tel" placeholder="<?php _e('Telefono (obbligatorio)', 'sprfc'); ?>" required />
 		<textarea ng-model="formData.message" placeholder="<?php _e('Messaggio', 'sprfc'); ?>"></textarea>
-		<input type="checkbox" id="privacy-<?php echo $type; ?>" required ng-model="formData.privacy" value="true" /><label for="privacy"><?php _e("Acconsento all'utilizzo dei dati inseriti secondo le finalità indicate dalla", 'sprfc'); ?> <a href="<?php ; ?>">privacy policy</a></label>
-		<input type="checkbox" id="marketing-<?php echo $type; ?>" ng-model="formData.marketing" value="true" /><label for="marketing"><?php _e("Acconsento all'utilizzo dei dati inseriti per l'invio di eventuali comunicazioni di marketing da parte del Titolare del Trattamento", 'sprfc'); ?></label>
+		<input type="checkbox" id="privacy-<?php echo $type; ?>" required ng-model="formData.privacy" value="true" /><label for="privacy-<?php echo $type; ?>"><?php _e("Acconsento all'utilizzo dei dati inseriti secondo le finalità indicate dalla", 'sprfc'); ?> <a href="<?php ; ?>">privacy policy</a></label>
+		<input type="checkbox" id="marketing-<?php echo $type; ?>" ng-model="formData.marketing" value="true" /><label for="marketing-<?php echo $type; ?>"><?php _e("Acconsento all'utilizzo dei dati inseriti per l'invio di eventuali comunicazioni di marketing da parte del Titolare del Trattamento", 'sprfc'); ?></label>
 		<button class="button" ng-disabled="contactForm.$invalid">
 			<span class="button__label"><?php _e('Invia', 'sprfc') ?></span>
 			<span class="button__alert" ng-class="{'button__alert--visible' : isContactSent}">
